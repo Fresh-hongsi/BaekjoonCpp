1a153ed887373aa6d1186f8a84394a1f7fed7619
Fresh-hongsi <78722713+Fresh-hongsi@users.noreply.github.com>
2022-07-31 20:14:42 +0900

    Update 1075-나누기.cpp

--- 1075-나누기.cpp
+++ 1075-나누기.cpp
@@ -1,6 +1,4 @@
 #include <iostream>
-#include <algorithm>
-#include <string>
 using namespace std;
 
 int main(void)
@@ -9,64 +7,37 @@ int main(void)
 	cin.tie(nullptr);
 	cout.tie(nullptr);
 
-	int n = 0;
-	int f = 0;
+	int N = 0;
+	cin >> N; //대상이 되는 수 입력
 
-	cin >> n >> f;
-	int a = n;
-	int b = n;
-	int counter_a = 0;
-	int counter_b = 0;
-	if (n % f == 0)
+	int F = 0;
+	cin >> F; //나누는 수 입력
+
+	int num_2 = (N % 100) / 10; //10의 자리수 숫자
+	int num_1 = (N % 10); //1의 자리수 숫자
+
+	
+	
+
+	for (int i = 0; i <= 99; i++)
 	{
-		cout << "00";
+		int	temp_n = N - ((num_2 * 10) + num_1); //temp_n은 N수의 뒤에 두 자리수를 0으로 만듦
+		
+		temp_n += i; //1씩 증가시켜보면서 나눌 수 있는 지 확인
+
+		if (temp_n % F == 0) //만약 나눌 수 있다면
+		{
+			if (i >= 0 && i <= 9) //한자리 수로 끝나면 0붙여야함
+			{
+				cout << '0' << i << endl;
+				break; //최소 수 찾았으므로 탈출
+			}
+
+			cout << i << endl; //한자리 수가 아니면 그냥 출력
+			break; //최소 수 찾았으므로 탈출
+		}
 	}
 
-	else
-	{
-		string a_fs = to_string(a);
-		int l_fa = a_fs.length();
-		while (a % f != 0)
-		{
-			
-			
-			a++;
-			counter_a++;
-			string a_s = to_string(a);
-			int l_a = a_s.length();
-			
-			if (l_fa!=l_a ||(a_s[l_a - 2] == '0' && a_s[l_a - 1] == '0'))
-				counter_a = -1;
-
-		}
-		string b_fs = to_string(b);
-		int l_fb = b_fs.length();
-		while (b % f != 0)
-		{
-			b--;
-			counter_b++;
-			string b_s = to_string(b);
-			int l_b = b_s.length();
-			if (l_fb!=l_b ||(b_s[l_fb - 2] == '0' && b_s[l_b - 1] == '0'))
-				counter_b = -1;
-		}
-
-		if ((counter_a>=0 && counter_b>=0)&&(counter_a > counter_b))
-		{
-			n = b;
-		}
-		else
-		{
-			n = a;
-		}
-
-		string s = to_string(n);
-		int l = s.length();
-
-		for (int i = l - 2; i < l; i++)
-		{
-			cout << s[i];
-		}
-	}
 	return 0;
+
 }
