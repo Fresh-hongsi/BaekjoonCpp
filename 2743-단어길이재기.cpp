bb187ea58bfb7db6444aec0b1171a490c290e7d3
Fresh-hongsi <78722713+Fresh-hongsi@users.noreply.github.com>
2022-08-06 14:15:28 +0900

    Create 2743-단어길이재기.cpp

---
+++ 2743-단어길이재기.cpp
@@ -0,0 +1,17 @@
+//String
+//Implementation
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+int main(void)
+{
+	string temp;
+	cin >> temp;
+
+	cout << temp.length();
+	return 0;
+
+}
