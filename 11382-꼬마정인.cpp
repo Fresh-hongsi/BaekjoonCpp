c83615ca0306e52a87263a563cf162c51804e476
Fresh-hongsi <78722713+Fresh-hongsi@users.noreply.github.com>
2022-08-14 12:41:16 +0900

    Create 11382-꼬마정인.cpp

---
+++ 11382-꼬마정인.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main(void)
+{
+	long long a = 0;
+	long long b = 0;
+	long long c = 0;
+	cin >> a >> b >> c;
+	long long d = a + b + c;
+
+	cout << d;
+	return 0;
+}
