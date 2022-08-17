907182d77fd05e0232f843236f85ce5c1958a765
Fresh-hongsi <78722713+Fresh-hongsi@users.noreply.github.com>
2022-08-17 20:56:55 +0900

    Create 2420-사파리월드.cpp

---
+++ 2420-사파리월드.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(void)
+{
+	long long a = 0;
+	long long b = 0;
+	cin >> a >> b;
+	long long c = abs(a - b);
+	cout << c;
+	return 0;
+}
