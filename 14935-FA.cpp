c8fe2a20ba4e6bcf4d0179f939b3e826a5bf487a
Fresh-hongsi <78722713+Fresh-hongsi@users.noreply.github.com>
2022-09-02 12:51:58 +0900

    Create 14935-FA.cpp

---
+++ 14935-FA.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	
+	string x;
+	cin >> x;
+
+	cout << "FA";
+
+	return 0;
+	
+
+
+
+}
