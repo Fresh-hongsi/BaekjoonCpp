349f709c0d34817965eb99f70812a4f40e002e77
Fresh-hongsi <78722713+Fresh-hongsi@users.noreply.github.com>
2022-10-01 14:19:41 +0900

    Create 5338-마이크로소프트로고.cpp

---
+++ 5338-마이크로소프트로고.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	string ans = "       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|\n";
+
+	cout << ans;
+	return 0;
+}
