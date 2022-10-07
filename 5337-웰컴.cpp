3c684cd36e49616d2c92aeef5bc98b15f8903b7c
Fresh-hongsi <78722713+Fresh-hongsi@users.noreply.github.com>
2022-10-07 19:09:41 +0900

    Create 5337-웰컴.cpp

---
+++ 5337-웰컴.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+
+	cout << ".  .   ." << endl;
+	cout << "|  | _ | _. _ ._ _  _" << endl;
+	cout << "|/\\|(/.|(_.(_)[ | )(/." << endl;
+
+	return 0;
+}
+
