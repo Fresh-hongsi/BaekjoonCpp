361481c10ef2c2d7a3577e75ffdd644eda5b8e33
Fresh-hongsi <78722713+Fresh-hongsi@users.noreply.github.com>
2022-10-16 13:18:59 +0900

    Create 3046-R2.cpp

---
+++ 3046-R2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+	int a = 0;
+	int mean = 0;
+
+	cin >> a >> mean;
+
+	int b = 0;
+
+	b = mean * 2 - a;
+	cout << b;
+
+	return 0;
+}
