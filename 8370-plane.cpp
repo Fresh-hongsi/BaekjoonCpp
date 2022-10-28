6f2912d5f14c9c2e247e7b93252634fe294e7d35
Fresh-hongsi <78722713+Fresh-hongsi@users.noreply.github.com>
2022-10-28 19:03:09 +0900

    Create 8370-plane.cpp

---
+++ 8370-plane.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+
+	int c = 0;
+	int d = 0;
+
+	cin >> a >> b >> c >> d;
+
+	cout << a * b + c * d;
+	return 0;
+}
