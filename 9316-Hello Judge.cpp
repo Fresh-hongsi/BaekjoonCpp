a6d501a96f20559de84102b40acee4fe16546f39
Fresh-hongsi <78722713+Fresh-hongsi@users.noreply.github.com>
2022-11-13 22:24:45 +0900

    Create 9316-Hello Judge.cpp

---
+++ 9316-Hello Judge.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+	int n = 0;
+	cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Hello World, Judge " << i+1 << "!" << '\n';
+	}
+
+	return 0;
+}
