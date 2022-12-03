76b930c14d77b01f1115ab3e1241def50faa3cf8
Fresh-hongsi <78722713+Fresh-hongsi@users.noreply.github.com>
2022-12-03 17:37:50 +0900

    Create 16394-홍익대학교.cpp

---
+++ 16394-홍익대학교.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+	int year = 1946;
+	int current = 0;
+	cin >> current;
+
+	int result = current - year;
+	cout << result;
+	return 0;
+}
