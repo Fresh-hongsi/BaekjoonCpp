f67ae162da7fd56966e6ed70dc9cbec9c77ab1a7
Fresh-hongsi <78722713+Fresh-hongsi@users.noreply.github.com>
2022-12-17 18:34:26 +0900

    Create 1997-트리 순회.cpp

---
+++ 1997-트리 순회.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+char a[50][2];
+
+void preorder(int N) {
+	if (N == -1) return;
+	cout << (char)(N + 'A');
+	preorder(a[N][0]);
+	preorder(a[N][1]);
+}
+
+void inorder(int N) {
+	if (N == -1) return;
+	inorder(a[N][0]);
+	cout << (char)(N + 'A');
+	inorder(a[N][1]);
+}
+
+void postorder(int N) {
+	if (N == -1) return;
+	postorder(a[N][0]);
+	postorder(a[N][1]);
+	cout << (char)(N + 'A');
+}
+
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		char x, y, z;
+		cin >> x >> y >> z;		// 알파벳 입력
+		x = x - 'A';		// 문자 -> 숫자 변환
+
+		if (y == '.') {		// 입력이 .이면 그 자리에 -1 대입
+			a[x][0] = -1;
+		}
+
+		else {
+			a[x][0] = y - 'A';
+		}
+
+		if (z == '.') {
+			a[x][1] = -1;
+		}
+
+		else {
+			a[x][1] = z - 'A';
+		}
+	}
+
+	preorder(0);
+	cout << '\n';
+
+	inorder(0);
+	cout << '\n';
+
+	postorder(0);
+	return 0;
+}
+
+
+
