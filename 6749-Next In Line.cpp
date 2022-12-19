d98609d7ed2cfc2eff16ec475140143ac741b8e7
Fresh-hongsi <78722713+Fresh-hongsi@users.noreply.github.com>
2022-12-19 15:06:15 +0900

    Create 6749-Next In Line.cpp

---
+++ 6749-Next In Line.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	cin >> a >> b;
+
+	int t = b - a;
+	c = b + t;
+	cout << c;
+	return 0;
+}
