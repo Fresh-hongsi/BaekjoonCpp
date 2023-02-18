5a770c12e36746051ba96c7cd9e92481c337fdfd
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2023-02-18 23:39:43 +0900

    Create 10189-Hook.cpp

---
+++ 10189-Hook.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << "#  # #### #### #  #" << endl;
+	cout << "#### #  # #  # # #" << endl;
+	cout << "#### #  # #  # # #" << endl;
+	cout << "#  # #### #### #  #" << endl;
+	return 0;
+}
+
+
