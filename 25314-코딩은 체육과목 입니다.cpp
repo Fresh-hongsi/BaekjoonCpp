0ed3ba6a33117bd023360fe8b404ade57705bfc7
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2023-02-27 19:25:32 +0900

    Create 25314-코딩은 체육과목 입니다.cpp

---
+++ 25314-코딩은 체육과목 입니다.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+	int t = 0;
+	cin >> t;
+
+	for (int i = 1; i <= t; i += 4)
+	{
+		cout << "long ";
+	}
+
+	cout << "int";
+	return 0;
+}
