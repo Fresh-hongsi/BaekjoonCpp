58f5d0e9b51caddaa1a4a3fd04f7832f688774b5
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2023-03-11 22:43:55 +0900

    Create 13597-Tri-du.cpp

---
+++ 13597-Tri-du.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+   int A, B;
+    cin >> A >> B;
+    cout << max(A , B) << endl;
+    return 0;
+}
