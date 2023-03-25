bb0a375926cfb8d4be631a019a7d67a064503d2a
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2023-03-25 15:55:56 +0900

    Create 24267-알고리즘 수업 - 알고리즘의 수행 시간 6.cpp

---
+++ 24267-알고리즘 수업 - 알고리즘의 수행 시간 6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	long long count = 0;
+	long long n = 0;
+	cin >> n;
+
+	
+
+	cout <<(n*(n-1)*(n-2))/6 << '\n';
+	cout << 3;
+	return 0;
+}
