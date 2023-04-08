0eadb0efa0c0f1c85eeb0ebbebb5ec9489157b69
Shihong Park <78722713+Fresh-hongsi@users.noreply.github.com>
2023-04-09 02:32:12 +0900

    Create 6778-Which Alien?.cpp

---
+++ 6778-Which Alien?.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int a = 0;
+	int b = 0;
+
+	cin >> a >> b;
+
+	if (a >= 3 && b <= 4)
+	{
+		cout << "TroyMartian" << '\n';
+	}
+
+	if (a <= 6 && b >= 2)
+	{
+		cout <<"VladSaturnian" << '\n';
+	}
+
+	if (a <= 2 && b <= 3)
+	{
+		cout << "GraemeMercurian" << '\n';
+	}
+
+	return 0;
+}
